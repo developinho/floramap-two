7ec45a652a59c7a270ea836a9eb9f3b0ed5c5579
colejohnsonline <colejohnson20@Cole-Johnsons-MacBook-Pro.local>
2014-10-09 18:16:40 -0500

    got view working and everything communicating YES

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ process.env.NODE_ENV = 'development';
 
 if (process.env.NODE_ENV === 'development') {
     app.set('port', process.env.PORT || 3000);
-    var logger = require('morgan');
-	app.use(logger); 
+    //var logger = require('morgan');
+	//app.use(logger); 
 	var bodyParser = require('body-parser');
     app.use(bodyParser.json())
     app.use(express.static(path.join(__dirname, 'public')));
 };
-
 
 app.get('/plants', plant.findAll);
 app.get('/plants/:id', plant.findById);
