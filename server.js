c15759d9beca75c3c6803561a964712326abe2d3
kellisonjk <kellisonjk@gmail.com>
2014-10-15 10:57:14 -0500

    Changing in server code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
     http = require('http'),
-    logger = require('morgan'),
+   // logger = require('morgan'),
     plant = require('./routes/plants');
 
 var app = express();
@@ -12,15 +12,13 @@ process.env.NODE_ENV = 'development';
 if (process.env.NODE_ENV === 'development') {
     app.set('port', process.env.PORT || 3000);
 
-    app.use(logger); 
+   // app.use(logger); 
     app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
     app.use(express.static(path.join(__dirname, 'public')));
 
 };
 
-=======
->>>>>>> origin/master
 app.get('/plants', plant.findAll);
 app.get('/plants/:id', plant.findById);
 app.post('/plants', plant.addPlant);
