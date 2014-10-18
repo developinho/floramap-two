841aa9068b9d01e3cbb5c6375f0e810863199e3d
colejohnsonline <colejohnson20@Cole-Johnsons-MacBook-Pro.local>
2014-10-18 16:15:29 -0500

    add passport.js dependency

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express'),
     plant = require('./routes/plants'),
     app = express();
 
+
+
 process.env.NODE_ENV = 'development';
 
 if (process.env.NODE_ENV === 'development') {
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV === 'development') {
     
     
    // app.use(logger); 
-    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,3 +29,24 @@ app.delete('/plants/:id', plant.deletePlant);
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 })
+
+var mongodb = require('mongodb');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://edgewater:floramap@ds047050.mongolab.com:47050/users');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
+});
+
+var kittySchema = mongoose.Schema({
+    name: String
+})
+
+var Kitten = mongoose.model('Kitten', kittySchema)
+
+var silence = new Kitten({ name: 'Silence' })
+console.log(silence.name)
+
