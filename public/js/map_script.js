edff3dcb2b3e0448f18170cc61f0c4a3317730c8
jonathanunes <jonathanunes92@gmail.com>
2014-10-20 03:52:43 -0500

    Details on facebook
    Updates by Jonatha, Kellison and Rafael

--- public/js/map_script.js
+++ public/js/map_script.js
@@ -119,8 +119,9 @@ function onLocationFound(e) {
 
     var popup = '<div class="popup_marker">You are <strong>here!</strong></div>';
    // var marker = L.marker(e.latlng, {icon: iconMarker}).addTo(map);
-    L.marker(e.latlng, {icon: iconMarker}).addTo(map).bindPopup(popup).openPopup();
+    var marker = L.marker(e.latlng, {icon: iconMarker}).addTo(map).bindPopup(popup).openPopup();
 
+    marker.on('click', markerClick);
 }
 
 function onLocationError(e) {
@@ -135,8 +136,8 @@ function markerClick(e) {
 
 function fillFieldsLatlng(coord) {
     $(document).ready(function() {
-        var latfield = $('#plant-view-form #latplant');
-        var lngfield = $('#plant-view-form #lngplant');
+        var latfield = $('#plant-view-form #latitude');
+        var lngfield = $('#plant-view-form #longitude');
 
         if ((latfield.length > 0) && (lngfield.length > 0)) {
             latfield.val(coord.lat);
